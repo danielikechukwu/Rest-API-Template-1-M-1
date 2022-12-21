dab9bd118987eeebea32b881ad35e5ed72851062
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-12-21 04:11:57 +0000

    Rest api template

---
+++ RestfulApiTemplate/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestfulApiTemplate.Models;
+
+namespace RestfulApiTemplate.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<StudentCourse>().HasKey(sc => new { sc.StudentId, sc.CourseId});
+
+            modelbuilder.Entity<StudentCourse>().HasOne<Student>(sc => sc.Student).WithMany(sc => sc.studentcourse);
+
+            modelbuilder.Entity<StudentCourse>().HasOne<Course>(sc => sc.Course).WithMany(sc => sc.studentcourse);
+        }
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<StudentCourse> studentsCourses { get; set; }
+    }
+}
---
+++ RestfulApiTemplate/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace RestfulApiTemplate.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public ICollection<StudentCourse> studentcourse { get; set; }
+
+
+    }
+}
